5395e3ba667781838e68f419f39c552f453f3f5e
miiin_sseong <wsc03002@naver.com>
2022-01-03 12:20:57 +0900

    apollo setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import { Global } from '@emotion/react';
 import { globalStyles } from '../src/commons/styles/globalStyles';
 import Header from '../src/components/commons/layout/header/Header.container';
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { ApolloClient, ApolloProvider, InMemoryCache, ApolloLink } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 
 // firebase
 // 송민성 : firebase appKey설정
@@ -18,14 +21,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const authService = getAuth();
 
 function MyApp({ Component, pageProps }: AppProps) {
+    // ApolloClient 셋팅
+    const uploadLink = createUploadLink({
+        uri: 'https://backend04-team.codebootcamp.co.kr/team02',
+    });
+    const client = new ApolloClient({
+        link: ApolloLink.from([uploadLink as ApolloLink]),
+        cache: new InMemoryCache(),
+    });
     return (
-        <>
+        <ApolloProvider client={client}>
             <Header />
             <Global styles={globalStyles} />
             <Component {...pageProps} />
-        </>
+        </ApolloProvider>
     );
 }
 
