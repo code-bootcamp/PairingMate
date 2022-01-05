37d3c16cc597d58e7b4fff2e46d10f9cefeca54c
hannah949494 <hyemin949494@gmail.com>
2022-01-05 21:46:58 +0900

    2022-01-05_정혜민 : [modify] layout index error fixed

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -2,11 +2,11 @@ import Footer from "./footer/Footer.container";
 import Header from "./header/Header.container";
 import { ILayoutProps } from "./layout.types";
 import { useRouter } from "next/router";
-import SubBanner from "./banner/sub/test";
+import SubBanner from "./banner/sub/SubBanner.container";
 
 const MAIN_ONLY = ["/"];
-const HIDDEN_HEADERS = [ "/login" , "/signup" ];
-const HIDDEN_FOOTERS = [ "/login" , "/signup" ];
+const HIDDEN_HEADERS = ["/login", "/signup"];
+const HIDDEN_FOOTERS = ["/login", "/signup"];
 
 const Layout = (props: ILayoutProps) => {
   const router = useRouter();
@@ -18,18 +18,16 @@ const Layout = (props: ILayoutProps) => {
     <>
       {!isMainOnly ? (
         <>
-
           {isHiddenHeader && isHiddenFooter ? (
             <div>{props.children}</div>
           ) : (
-            <>   
-            <Header />
+            <>
+              <Header />
               <SubBanner />
               <div>{props.children}</div>
-            <Footer />
+              <Footer />
             </>
-          )
-        }
+          )}
         </>
       ) : (
         <div>{props.children}</div>
