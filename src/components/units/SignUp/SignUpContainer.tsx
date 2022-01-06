d080da169e7ba7d8ef331bc22a84155abd7888f3
kwontaewook <taewoogie@gmail.com>
2022-01-06 23:03:22 +0900

    2022-01-06_권태욱 : [modify] 회원가입 절차 1 페이지 디자인 완료

--- src/components/units/SignUp/SignUpContainer.tsx
+++ src/components/units/SignUp/SignUpContainer.tsx
@@ -1,9 +1,24 @@
+import { useRouter } from "next/router";
 import SignUpUI from "./SignUpPresenter";
 
 const SignUp = () => {
+    const router = useRouter();
 
+    const onClickSignupUser = () => {
+        alert("일반회원으로 가입 시작합니다.")
+        // router.push("");
+    }
+
+    const onClickSignupCompany = () => {
+        alert("기업회원으로 가입 시작합니다.");
+        // router.push("");
+    }
+    
     return (
-        <SignUpUI />
+        <SignUpUI
+            onClickSignupUser={onClickSignupUser}
+            onClickSignupCompany={onClickSignupCompany}
+        />
     )
 }
 export default SignUp;
--- src/components/units/SignUp/SignUpPresenter.tsx
+++ src/components/units/SignUp/SignUpPresenter.tsx
@@ -1,6 +1,8 @@
 import * as S from "./SignUpStyles";
+import { SignUpUIProps } from "./SignUpTypes";
 
-const SignUpUI = () => {
+
+const SignUpUI = (props: SignUpUIProps) => {
 
     return (
         <S.SignUpWrapper>
@@ -8,16 +10,17 @@ const SignUpUI = () => {
                 <S.SignUpHeadTitle>당신은<br />어떤
                 <strong> Mate </strong> 이신가요?
                 </S.SignUpHeadTitle>
-
                 <S.SignUpBodyTitle>원하는 회원가입 유형을 선택해주세요</S.SignUpBodyTitle>
-
-                <S.SignUpRadioWrapper>
-                    <S.SignUpFooterTitle>Mate를 찾고 싶어요</S.SignUpFooterTitle>
-                    <S.SignUpRadio type="radio" name="SignupType" checked /> 일반회원으로 회원가입
-
-                    <S.SignUpFooterTitle>PairingMate와 Mate가 되고 싶어요</S.SignUpFooterTitle>
-                    <S.SignUpRadio type="radio" name="SignupType" /> 기업회원으로 회원가입
-                </S.SignUpRadioWrapper>
+                <S.SignUpButtonWrapper>
+                    <div>
+                        <S.SignUpFooterTitle>Mate를 찾고 싶어요</S.SignUpFooterTitle>
+                        <S.SignUpButton onClick={props.onClickSignupUser}> 일반회원으로 회원가입 </S.SignUpButton>
+                    </div>
+                    <div>
+                        <S.SignUpFooterTitle>PairingMate와 Mate가 되고 싶어요</S.SignUpFooterTitle>
+                        <S.SignUpButton onClick={props.onClickSignupCompany}> 기업회원으로 회원가입 </S.SignUpButton>
+                    </div>
+                </S.SignUpButtonWrapper>
             </S.SignUpWrap>
         </S.SignUpWrapper>
     )
