2ee66079469d682c90f16e4a9a53b5286119d507
takk2 <dudxkr94@gmail.com>
2022-01-06 10:37:48 +0900

    2022-01-04_김영탁 : [update]지도기능 탑재, UI편집

--- src/components/units/FitnessCenters/FitnessCentersPresenter.tsx
+++ src/components/units/FitnessCenters/FitnessCentersPresenter.tsx
@@ -1,5 +1,31 @@
-const FitnessCentersUI = () => {
+import { Inner } from "../../../commons/styles/common";
+import * as F from "./FitnessCentersStyles"
 
-    return <></>
+const FitnessCentersUI = (props) => {
+
+    return (
+        <>
+            <F.Wrapper>
+                <F.Map id="map"/>
+                <F.List>
+                    <F.TitleBox>
+                        <F.Title>내 주변 피트니스 센터</F.Title>
+                    </F.TitleBox>
+                    <div>
+                    {props.myKeyword &&
+                        props.myKeyword.map((el: any, index) => (
+                        <div key={el.id}>
+                            <div onClick={props.newMarker(index)}>업체명: {el.place_name}</div>
+                            <div>주소: {el.address_name}</div>
+                            <div>전화번호: {el.phone}</div>
+                            <a href={el.place_url}>상세보기</a>
+                            
+                        </div>
+                        ))}
+                    </div>
+                </F.List>
+            </F.Wrapper>
+        </>
+    )
 }
 export default FitnessCentersUI;
