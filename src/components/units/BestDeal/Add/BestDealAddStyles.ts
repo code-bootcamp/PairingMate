bc0267cbd69f041145073a0dc5bb12306f62e8a2
takk2 <dudxkr94@gmail.com>
2022-01-07 10:29:38 +0900

    2022-01-07_김영탁 : [modify]bestdeal(유효성 검사 추가, UI수정)

--- src/components/units/BestDeal/Add/BestDealAddStyles.ts
+++ src/components/units/BestDeal/Add/BestDealAddStyles.ts
@@ -1,1 +1,13 @@
-// Css 작성
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  height: auto;
+  margin: 0 auto;
+`;
+export const Header = styled.div`
+  width: 100%;
+  height: 50px;
+  margin-left: 50px;
+`;
