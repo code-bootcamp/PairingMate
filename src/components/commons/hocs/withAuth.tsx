23b07f14106bbdf11da8fa302895427b06fb0f83
kwontaewook <taewoogie@gmail.com>
2022-01-14 16:33:00 +0900

    2022-01-14_권태욱 : [modify] 카테고리별 권한분기 적용

--- src/components/commons/hocs/withAuth.tsx
+++ src/components/commons/hocs/withAuth.tsx
@@ -6,9 +6,9 @@ export const withAuth = <P extends {}>(Component: ComponentType<P>) => (props: P
     const router = useRouter();
 
     useEffect(() => {
-      if (localStorage.getItem("refreshToken") === "false") {
-        Modal.error({content:"로그인 한 사람만 입장 가능합니다!!! 로그인을 먼저 해주세요!!"});
-        router.push("/login");
+      if (!localStorage.getItem("refreshToken")) {
+        Modal.warning({title:"접근 제한", content:"로그인 한 사람만 입장 가능합니다!"});
+        router.push("/");
       }
     }, []);
 
