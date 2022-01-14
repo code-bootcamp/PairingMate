73931fd386e7b324b291382e4a83b5953cd58ece
hannah949494 <hyemin949494@gmail.com>
2022-01-14 20:40:14 +0900

    2022-01-14_정혜민: [update] mypage index 이동

--- pages/mypage/user/index.tsx
+++
@@ -1,8 +0,0 @@
-import { withAuth } from "../../../src/components/commons/hocs/withAuth";
-import MyPage from "../../../src/components/units/MyPage/user/MyPageContainer";
-
-const mypage = () => {
-  return <MyPage />;
-};
-
-export default withAuth(mypage);
